e9bea4fc997187f9c146a395dcfefa07c2176b9f
Taylor Allen <taylorallen0913@gmail.com>
2022-11-01 16:58:23 -0700

    test

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+numbers = []
 
-# ******************************
-# Make your Code
-# ******************************
+for i in range(5):
+    numbers.append(int(input()))
+
+avg = sum(numbers) / len(numbers)
+
+for num in numbers:
+    if num > avg:
+        print(num, end=' ')
